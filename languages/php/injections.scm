4204aa350cca05a87479b420e9adb38a28815381
Laurent Goussard <loranger@free.fr>
2025-08-19 14:47:39 +0200

    Improve PHP SQL injection grammar for broader detection
    Expanded the SQL keyword regex to include more query-related words and
    ensure a non-word character follows the match to prevent partial word
    matches.

--- languages/php/injections.scm
+++ languages/php/injections.scm
@@ -3,7 +3,7 @@
  (#set! injection.combined))
 
 ((string_value) @injection.content
- (#match? @injection.content "(?i)^\\s*(select|update|insert|delete|show|begin|use|drop|truncate|create|alter)")
+    (#match? @injection.content "(?i)^\\s*(select|update|insert|delete|show|begin|use|drop|truncate|create|alter|and|from|join|where|group|order)\\W")
  (#set! injection.language "sql"))
 
 ((comment) @injection.content
