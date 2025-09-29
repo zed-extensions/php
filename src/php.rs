15aaea9de0988c61f40e7ceaa486aa7de109f2ff
MrSubidubi <75036051+MrSubidubi@users.noreply.github.com>
2025-09-29 11:54:41 +0200

    Remove Windows path workaround, more fixes

--- src/php.rs
+++ src/php.rs
@@ -1,22 +1,32 @@
 mod language_servers;
+mod xdebug;
 
 use std::fs;
-use std::path::PathBuf;
 use zed::CodeLabel;
-use zed_extension_api::{self as zed, serde_json, LanguageServerId, Result};
+use zed_extension_api::{
+    self as zed, serde_json, DebugConfig, DebugScenario, LanguageServerId, Result,
+    StartDebuggingRequestArgumentsRequest,
+};
 
-use crate::language_servers::{Intelephense, Phpactor};
+use crate::{
+    language_servers::{Intelephense, PhpTools, Phpactor},
+    xdebug::XDebug,
+};
 
 struct PhpExtension {
+    phptools: Option<PhpTools>,
     intelephense: Option<Intelephense>,
     phpactor: Option<Phpactor>,
+    xdebug: XDebug,
 }
 
 impl zed::Extension for PhpExtension {
     fn new() -> Self {
         Self {
+            phptools: None,
             intelephense: None,
             phpactor: None,
+            xdebug: XDebug::new(),
         }
     }
 
@@ -26,6 +36,10 @@ impl zed::Extension for PhpExtension {
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
         match language_server_id.as_ref() {
+            PhpTools::LANGUAGE_SERVER_ID => {
+                let phptools = self.phptools.get_or_insert_with(PhpTools::new);
+                phptools.language_server_command(language_server_id, worktree)
+            }
             Intelephense::LANGUAGE_SERVER_ID => {
                 let intelephense = self.intelephense.get_or_insert_with(Intelephense::new);
                 intelephense.language_server_command(language_server_id, worktree)
@@ -43,22 +57,22 @@ impl zed::Extension for PhpExtension {
                     let php_path = worktree
                         .which("php")
                         .ok_or("Could not find PHP in path! PHP needs to be installed for running phpactor on Windows")?;
-                    // get abs_phparctor_path
-                    let abs_phparctor_path = fs::canonicalize(&phpactor_path).or_else(|_| {
-                        let path = PathBuf::from(worktree.root_path()).join(&phpactor_path);
-                        if fs::exists(&path).is_ok_and(|exists| exists) {
-                            Ok(path)
-                        } else {
-                            Err("Could not resolve phpactor path!")
-                        }
-                    })?;
+
+                    let abs_phpactor_path = std::env::current_dir()
+                        .map_err(|_| "Could not get current directory")?
+                        .join(&phpactor_path);
+
+                    if !fs::exists(&abs_phpactor_path).map_or(false, |exists| exists) {
+                        return Err(format!(
+                            "Could not resolve phpactor path {:?}!",
+                            phpactor_path
+                        ));
+                    };
 
                     Ok(zed::Command {
                         command: php_path,
                         args: vec![
-                            zed_ext::sanitize_windows_path(abs_phparctor_path)
-                                .to_string_lossy()
-                                .to_string(),
+                            abs_phpactor_path.to_string_lossy().into(),
                             "language-server".into(),
                         ],
                         env: Default::default(),
@@ -80,6 +94,11 @@ impl zed::Extension for PhpExtension {
         language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<Option<serde_json::Value>> {
+        if language_server_id.as_ref() == PhpTools::LANGUAGE_SERVER_ID {
+            if let Some(phptools) = self.phptools.as_mut() {
+                return phptools.language_server_workspace_configuration(worktree);
+            }
+        }
         if language_server_id.as_ref() == Intelephense::LANGUAGE_SERVER_ID {
             if let Some(intelephense) = self.intelephense.as_mut() {
                 return intelephense.language_server_workspace_configuration(worktree);
@@ -101,28 +120,44 @@ impl zed::Extension for PhpExtension {
             _ => None,
         }
     }
+    fn dap_request_kind(
+        &mut self,
+        adapter_name: String,
+        config: serde_json::Value,
+    ) -> Result<StartDebuggingRequestArgumentsRequest, String> {
+        if adapter_name != XDebug::NAME {
+            return Err(format!(
+                "PHP extension does not support unknown adapter in `dap_request_kind`: {adapter_name} (supported: [{}])",
+                XDebug::NAME
+            ));
+        }
+        self.xdebug.dap_request_kind(&config)
+    }
+    fn dap_config_to_scenario(&mut self, config: DebugConfig) -> Result<DebugScenario, String> {
+        if config.adapter != XDebug::NAME {
+            return Err(format!(
+                "PHP extension does not support unknown adapter in `dap_config_to_scenario`: {} (supported: [{}])",
+                config.adapter, XDebug::NAME
+            ));
+        }
+        self.xdebug.dap_config_to_scenario(config)
+    }
+    fn get_dap_binary(
+        &mut self,
+        adapter_name: String,
+        config: zed_extension_api::DebugTaskDefinition,
+        user_provided_debug_adapter_path: Option<String>,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<zed_extension_api::DebugAdapterBinary, String> {
+        if config.adapter != XDebug::NAME {
+            return Err(format!(
+                "PHP extension does not support unknown adapter in `get_dap_binary`: {} (supported: [{}])",
+                adapter_name, XDebug::NAME
+            ));
+        }
+        self.xdebug
+            .get_binary(config, user_provided_debug_adapter_path, worktree)
+    }
 }
 
 zed::register_extension!(PhpExtension);
-
-/// Extensions to the Zed extension API that have not yet stabilized.
-mod zed_ext {
-    /// Sanitizes the given path to remove the leading `/` on Windows.
-    ///
-    /// On macOS and Linux this is a no-op.
-    ///
-    /// This is a workaround for https://github.com/bytecodealliance/wasmtime/issues/10415.
-    pub fn sanitize_windows_path(path: std::path::PathBuf) -> std::path::PathBuf {
-        use zed_extension_api::{current_platform, Os};
-
-        let (os, _arch) = current_platform();
-        match os {
-            Os::Mac | Os::Linux => path,
-            Os::Windows => path
-                .to_string_lossy()
-                .to_string()
-                .trim_start_matches('/')
-                .into(),
-        }
-    }
-}
